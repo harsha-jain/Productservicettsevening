7f01ebde890cbe10173eea5ce1d65b9dba482380
harsha-jain <harshajain98@gmail.com>
2023-10-10 19:03:36 +0530

    Application propertires error

--- src/main/java/com/example/productservicettsevening/ProductservicettseveningApplication.java
+++ src/main/java/com/example/productservicettsevening/ProductservicettseveningApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProductservicettseveningApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ProductservicettseveningApplication.class, args);
     }
 
